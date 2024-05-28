1083814f63f28d0cf42b8b05fa02f90fd3ad503c
Ricardo Sanchez <ricsanchez10@gmail.com>
2024-05-28 17:25:59 -0300

    Aritimetica

---
+++ PYTHON/Aula007a/aula07a.py
@@ -0,0 +1,9 @@
+n1 = int(input('Digite o primeiro numero: '))
+n2 = int(input("Digite o Segundo numero: "))
+s = n1 + n2 
+sub = n1 - n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2 
+p = n1 ** n2
+print('A soma é : {} \n A Subtração é : {}\n A Multiplicação é : {} \n A Divisão é : {:.3} \n A Divisão Inteira é : {} \n Potência é : {}'.format(s, sub, m, d, di, p))
